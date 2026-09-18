363bfa1ceb0716ff5cbbce270df144df543c66c2
oniyemorolake-dev <oniyemorolake@gmail.com>
2026-09-18 16:30:28 -0600

    fixing the error

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <div>
-      <p>Your Name</p>
-      <Link href="https://github.com/your-username/cprg306-assignments">
+      <p>Morolake Oniye</p>
+      <Link href="https://github.com/oniyemorolake-dev/cprg306-assignments">
         GitHub Repository
       </Link>
     </div>
