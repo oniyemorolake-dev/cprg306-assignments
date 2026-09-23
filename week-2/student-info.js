8932d6e2a0bf7f4cb6fb03a8924357c07d101af9
oniyemorolake-dev <oniyemorolake@gmail.com>
2026-09-23 08:08:14 -0600

    updated github username

--- week-2/student-info.js
+++ week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <div>
       <p>Morolake Oniye</p>
-      <Link href="https://github.com/your-username/cprg306-assignments">
+      <Link href="https://github.com/oniyemorolake-dev/cprg306-assignments">
         GitHub Repository
       </Link>
     </div>
