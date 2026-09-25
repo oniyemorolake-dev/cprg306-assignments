ae1138cdce1bdba9cf8f36d96387a3e97f3bca02
oniyemorolake-dev <oniyemorolake@gmail.com>
2026-09-25 15:12:05 -0600

    Assignment 2 updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2</Link>
-      <Link href="/week-3">Week 3</Link>
-      <p>Assignments and projects for this course.</p>
-    </main>
+    <main className="min-h-screen bg-black-50 p-8">
+  <h1 className="mb-6 text-4xl font-bold text-green-800">
+    CPRG 306: Web Development 2 - Assignments
+  </h1>
+
+  <div className="flex gap-4">
+    <Link href="/week-2">Week 2</Link>
+    <Link href="/week-3">Week 3</Link>
+  </div>
+
+  <p className="mt-6 text-green-700">
+    Assignments and projects for this course.
+  </p>
+</main>
   );
 }
